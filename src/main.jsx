58efe98a920de7281070c303c8660086b1afb9a2
MartinezFabian <FabianMartinezDev@gmail.com>
2023-07-17 16:27:05 -0300

    Feat: Add font and global CSS styles

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,8 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { GifFinderApp } from "./GifFinderApp";
+import "normalize.css";
+import "./styles.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
