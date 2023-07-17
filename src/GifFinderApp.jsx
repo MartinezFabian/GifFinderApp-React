1c275dcf46939de5dfcde3a16b842fff7717f258
MartinezFabian <FabianMartinezDev@gmail.com>
2023-07-17 18:06:22 -0300

    Feat: Add functionality to display the list of categories

--- src/GifFinderApp.jsx
+++ src/GifFinderApp.jsx
@@ -1,7 +1,17 @@
+import { useState } from 'react';
+
 export const GifFinderApp = () => {
+  const [categories, setCategories] = useState(['category 1', 'category 2', 'category 3']);
+
   return (
     <>
       <h1>Buscador de GIFs</h1>
+
+      <ul>
+        {categories.map((category) => (
+          <li key={category}> {category}</li>
+        ))}
+      </ul>
     </>
   );
 };
