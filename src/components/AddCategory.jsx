cb420f33ea72da61766dd44e39341a45fd98d6a6
MartinezFabian <FabianMartinezDev@gmail.com>
2023-07-23 20:18:57 -0300

    Feat: Add AddCategory.module.css for component styles

--- src/components/AddCategory.jsx
+++ src/components/AddCategory.jsx
@@ -1,4 +1,5 @@
 import { useState } from 'react';
+import styles from './AddCategory.module.css';
 
 export const AddCategory = ({ onNewCategory }) => {
   const [inputValue, setInputValue] = useState('');
@@ -20,9 +21,30 @@ export const AddCategory = ({ onNewCategory }) => {
   };
 
   return (
-    <form onSubmit={onFormSubmit}>
-      <input type="text" placeholder="Buscar Gif" onChange={onInputChange} value={inputValue} />
-      <input type="submit" value="Buscar" onSubmit={onFormSubmit} />
+    <form onSubmit={onFormSubmit} className={styles.form}>
+      <input
+        type="text"
+        placeholder="Buscar Gif"
+        onChange={onInputChange}
+        value={inputValue}
+        className={styles.form__input}
+      />
+      <button type="submit" onSubmit={onFormSubmit} className={styles.form__button}>
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          fill="none"
+          viewBox="0 0 24 24"
+          strokeWidth={1.5}
+          stroke="white"
+          className={styles.form__icon}
+        >
+          <path
+            strokeLinecap="round"
+            strokeLinejoin="round"
+            d="M21 21l-5.197-5.197m0 0A7.5 7.5 0 105.196 5.196a7.5 7.5 0 0010.607 10.607z"
+          />
+        </svg>
+      </button>
     </form>
   );
 };
