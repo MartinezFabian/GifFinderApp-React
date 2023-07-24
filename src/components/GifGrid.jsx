5d2a836c9f7703d83d9a4b33103f900c2b8e90f0
MartinezFabian <FabianMartinezDev@gmail.com>
2023-07-23 21:33:53 -0300

    Feat: Add GifGrid.module.css for component styles

--- src/components/GifGrid.jsx
+++ src/components/GifGrid.jsx
@@ -1,13 +1,14 @@
 import { useFetchGifs } from '../hooks/useFetchGifs';
 import { GifCard } from './GifCard';
+import styles from './GifGrid.module.css';
 
 export const GifGrid = ({ category }) => {
   const { gifs, isLoading } = useFetchGifs(category);
 
   return (
-    <section>
-      <h2>{category}</h2>
-      <div>
+    <section className={styles.category}>
+      <h2 className={styles.category__heading}>{category}</h2>
+      <div className={styles.category__grid}>
         {gifs.map(({ id, title, url }) => (
           <GifCard key={id} title={title} url={url}></GifCard>
         ))}
